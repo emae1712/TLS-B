8ca88c79053cd2d081d82e774eface0498420549
emae1712 <melissa.aechegaray@gmail.com>
2021-03-10 17:33:54 -0500

    files without console

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,21 @@ import Logo from '../img/pwc-logo.jpg';
 import Avatar from '../img/avatar.jpg';
 import '../styles/Header.scss';
 
-const Header = () => {
-  console.log('Header');
-  return (
-    <header className="header__container">
-      <div className="logo-logout">
-        <NavLink to="/" className="logo"><img src={Logo} alt="pwc" /></NavLink>
-        <div className="logout">
-          <button type="button" onClick={() => auth.signOut()}>Cerrar sesión</button>
-          <img src={Avatar} alt="avatar" />
-        </div>
+const Header = () => (
+  <header className="header__container">
+    <div className="logo-logout">
+      <NavLink to="/" className="logo"><img src={Logo} alt="pwc" /></NavLink>
+      <div className="logout">
+        <button type="button" onClick={() => auth.signOut()}>Cerrar sesión</button>
+        <img src={Avatar} alt="avatar" />
       </div>
-      <nav>
-        <NavLink activeClassName="activeRoute" activeStyle={{ color: '#ffffff' }} exact to="/">  Registrar consulta </NavLink>
-        <NavLink activeClassName="activeRoute" activeStyle={{ color: '#ffffff' }} to="/consultasVigentes">  Consultas vigentes </NavLink>
-        <NavLink activeClassName="activeRoute" activeStyle={{ color: '#ffffff' }} to="/consultasHistoricas">  Consultas históricas </NavLink>
-        <NavLink activeClassName="activeRoute" activeStyle={{ color: '#ffffff' }} to="/informacion">  Información de interés </NavLink>
-      </nav>
-    </header>
-  );
-};
+    </div>
+    <nav>
+      <NavLink activeClassName="activeRoute" activeStyle={{ color: '#ffffff' }} exact to="/">  Registrar consulta </NavLink>
+      <NavLink activeClassName="activeRoute" activeStyle={{ color: '#ffffff' }} to="/consultasVigentes">  Consultas vigentes </NavLink>
+      <NavLink activeClassName="activeRoute" activeStyle={{ color: '#ffffff' }} to="/consultasHistoricas">  Consultas históricas </NavLink>
+      <NavLink activeClassName="activeRoute" activeStyle={{ color: '#ffffff' }} to="/informacion">  Información de interés </NavLink>
+    </nav>
+  </header>
+);
 export default Header;
