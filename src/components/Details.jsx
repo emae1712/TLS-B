b1d3a617f1568c7760d76f252191945ee285d160
emae1712 <melissa.aechegaray@gmail.com>
2021-03-11 08:09:02 -0500

    details data to 50%

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import {
+  Link, useParams,
+} from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
 import { BsFileEarmarkArrowDown } from 'react-icons/bs';
@@ -8,23 +11,38 @@ import Avatar from '../img/avatar.jpg';
 import Header from './Header';
 import '../styles/App.scss';
 import '../styles/Details.scss';
+import { db } from '../firebase/fb-configuration';
 
-const Detail = (props) => {
-  // const { id } = props;
-  console.log(props);
-
+const Detail = () => {
+  const { id } = useParams();
+  const [client, setClient] = useState([]);
+  useEffect(() => {
+    db.collection('queries').doc(id).get()
+      .then((doc) => setClient(doc.data()));
+  }, [id]);
   return (
     <>
       <Header />
       <section className="detail__container">
         <div className="detail-card">
-          <Link to="/consultasVigentes"><AiOutlineArrowLeft /></Link>
+          {
+          client.status === 'Resuelta'
+            ? <Link to="/consultasHistoricas"><AiOutlineArrowLeft /></Link>
+
+            : <Link to="/consultasVigentes"><AiOutlineArrowLeft /></Link>
+          }
           <div className="detail">
             <h2>Detalle de consulta</h2>
             <p>ID 132</p>
             <div>
-              <p>Fecha: 23/03/2021</p>
-              <p>Tema: Modificación tributaria</p>
+              <p>
+                Fecha:
+                {client.status }
+              </p>
+              <p>
+                Tema:
+                { client.sector }
+              </p>
               <p>Sublo Tributario</p>
               <p>Socio a cargo: Cristina Yang Aval</p>
               <p>Gerente a cargo: Maria Alejendra Rivera Lopez</p>
@@ -35,7 +53,7 @@ const Detail = (props) => {
             <table>
               <tr>
                 <th>Firstname</th>
-                <th>Lastname</th>
+                <th>{ client.status }</th>
               </tr>
               <tr>
                 <td>Total</td>
@@ -54,21 +72,15 @@ const Detail = (props) => {
                 <div className="name-consult">
                   <p>Maria Fernanda Cevedo</p>
                   <p>
-                    Fecha: 23/03/2021 Hora: 10:56 Sublos Auditoria
+                    Fecha:
+                    {' '}
+                    {client.status}
+                    {' '}
+                    Sublos Auditoria
                   </p>
                 </div>
                 <p className="consult-text">
-                  Revisar el Decreto 90, considero que en una norma de emisión no se pueden medir todos los parámetros contaminantes,
-                  frente a ello existe la alternativa de someter a las descargas a test ecotoxicológicos, a través de un principio que
-                  lamentablemente no aparece en la modificación de la norma, como es el de no descargar efluentes tóxicos agudos a los
-                  cuerpos de agua superficial. En mi experiencia efluentes que cumplen con la norma de emisión, no necesariamente protegen
-                  la calidad ambiental, que es el objetivo de esta norma, por lo que agregar la obligación de realizar test de toxicidad con
-                  el efluente final debiera ser un requisito para garantizar el cumplimiento de la norma de emisión y que contribuye a su
-                  objetivo. Solo aquellas fuentes emisoras que tienen RCA donde se requieren ensayos de toxicidad en sus programas de vigilancia
-                  lo realizan, ¿por que no extender esta obligación a todas las fuentes emisoras que emitan mas de un volumen anual que determine
-                  la norma (de la misma forma que el volumen descargado determina el numero de monitoreos mensuales que se deben ejecutar?). Así nos
-                  aseguramos que efluentes que cumplen con los parámetros físicos y químicos exigidos son también no-tóxicos para el cuerpo receptor,
-                  a forma que regulaciones mas avanzadas, como por ejemplo, la canadiense, lo exigen. Ser sostenibles requiere de esfuerzos adicionales.
+                  {client.query}
                 </p>
               </div>
             </div>
@@ -105,7 +117,6 @@ const Detail = (props) => {
             </table>
 
           </div>
-
         </div>
         <div className="add-consult">
           <p>Formulario</p>
