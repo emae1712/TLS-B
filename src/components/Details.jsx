cd3dc8f73a03c5b4f346e4e60d135d801ee2b335
VeroFlores <veritocalla@gmail.com>
2021-03-11 11:54:13 -0500

    create a subcollection to store comments

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -14,54 +14,13 @@ import { AuthContext } from '../Context/Auth';
 import { storage, db } from '../firebase/fb-configuration';
 import Avatar from '../img/avatar.jpg';
 import Header from './Header';
+import TableDetail from './TableDetail';
 import '../styles/App.scss';
 import '../styles/Details.scss';
 
 const Detail = () => {
   //
-  const initialValue = {
-    sector: '',
-    query: '',
-  };
-  const [values, setValues] = useState(initialValue);
-  const [files, setFiles] = useState([]);
-  // const [links, setLinks] = useState([]);
-  const { currentUser } = useContext(AuthContext);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
-  const onFileChange = (e) => {
-    for (let i = 0; i < e.target.files.length; i += 1) {
-      const newFile = e.target.files[i];
-      newFile.id = Math.random();
-      // add an "id" property to each File object
-      setFiles((prevState) => [...prevState, newFile]);
-    }
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    db.collection('queries').add({
-      user: currentUser.uid,
-      time: new Date(),
-      adviser: 'Regina Díaz',
-      status: 'pendiente',
-      ...values,
-    }).then((docRef) => {
-      const promisesArr = [];
-      files.forEach((file) => {
-        const storageRef = storage.ref(`doc/${file.name}`);
-        const fileRef = storageRef.child(file.name);
-        const promise = fileRef.put(file).then(() => fileRef.getDownloadURL());
-        promisesArr.push(promise);
-      });
-      Promise.all(promisesArr).then((arr) => db.collection('queries').doc(docRef.id).update({
-        imgs: arr,
-      }));
-      setValues(initialValue);
-      setFiles([]);
-    });
-  };
+
   //
   // base de datos del detalle de la consulta
   const { id } = useParams();
@@ -170,44 +129,7 @@ const Detail = () => {
           </div>
         </div>
         <div className="add-consult">
-          <div className="col-6">
-            <Form action="post" onSubmit={handleSubmit} className="m-4">
-              <Form.Group as={Row} controlId="formHorizontalEmail" className="m-2 d-flex justify-start" />
-              <Form.Group className="mx-auto col-10 ">
-                <Form.Group as={Row} md="8" controlId="formHorizontalEmail" />
-                <Form.Group as={Row} controlId="formHorizontalPassword" />
-                <Form.Group as={Row} controlId="formHorizontalPassword">
-                  <Form.Label column sm={4}>
-                    Consulta realizada
-                  </Form.Label>
-                  <Col sm={6}>
-                    <Form.Control
-                      as="textarea"
-                      rows={3}
-                      onChange={handleChange}
-                      type="text"
-                      name="query"
-                      value={values.query}
-                    />
-                  </Col>
-                </Form.Group>
-              </Form.Group>
-
-              <Form.Group as={Row} controlId="formHorizontalEmail" className="mx-auto col-10 ">
-                <Form.Label column sm={4} />
-                <Col>
-                  <Form.File id="exampleFormControlFile1" name="doc1" onChange={onFileChange} />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row} controlId="formHorizontalCheck" className="d-flex align-items-center mx-auto col-10  ">
-                <Col sm={{ span: 6, offset: 5 }}>
-                  <button type="submit" className="btn btn-secondary">Enviar</button>
-                </Col>
-              </Form.Group>
-
-            </Form>
-          </div>
-
+          <TableDetail querieId={id} />
         </div>
       </section>
     </>
